8fd695c89aa93931cb3f8339508c35ace6de5158
CodeNameMS <86137847+CodeNameMS@users.noreply.github.com>
2021-09-07 17:27:47 +0900

    Day01
    Java 공부 및 Test

---
+++ Day01/src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+// ctrl + + : 폰트 확대
+// ctrl + - : 폰트 축소
+// ctrl + s : 저장
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		
+		// ctrl + F11 : 실행
+	}
+}
